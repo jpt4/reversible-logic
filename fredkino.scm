4582b6655b56ef371594e58543bdb42da155b098
James P. Torre, IV <torreaiworks01@gmail.com>
2015-06-09 22:10:41 +0000

    Rearrangement

--- fredkino.scm
+++ fredkino.scm
@@ -1,40 +1,41 @@
-;20130123 JPTIV
-;Rev. 20131118
-;Rev. 20131122
+;;  20130123 JPTIV
+;;  Rev. 20131118
+;;  Rev. 20131122
 
-(load "mk.scm")
-(load "minikanren.scm")
+(load "miniKanren-with-symbolic-constraints/mk.scm")
 
-;Relational 3-field Fredkin gate. Controlled swap semantics. Preserves
-;argument positions if first field is #f. Swaps last two otherwise.
+;;  Relational 3-field Fredkin gate. Controlled swap
+;;  semantics. Preserves argument positions if first field is
+;;  #f. Swaps last two otherwise.
 (define 3f-o
   (lambda (a b c d e f)
     (conde
       [(=/= a #f) (== `(,a ,b ,c) `(,d ,f ,e))]
       [(== a #f) (== `(,a ,b ,c) `(,d ,e ,f))])))
 
-;Simulated and
+;;  Simulated and
 (define and-o
   (lambda (a b c)
     (fresh (d e f)
       (3f-o a a b d e f)
       (== c e))))
 
-;Simulated or
+;;  Simulated or
 (define or-o
   (lambda (a b c)
     (fresh (d e f)
       (3f-o a b a d e f)
       (== c e))))
 
-;Simulated not, b ranges over {#t #f}.
+;;  Simulated not, b ranges over {#t #f}.
 (define not-o
   (lambda (a b)
     (fresh (d e f)
       (3f-o a #t #f d e f)
       (== b e))))
 
-;Simulated nor, demonstrates classical logical completeness of {Friedkin gates with #t/#f constants}.
+;;  Simulated nor, demonstrates classical logical completeness of
+;;  {Friedkin gates with #t/#f constants}.
 (define nor-o
   (lambda (a b c)
     (fresh (d e f g h i)
@@ -42,7 +43,8 @@
       (3f-o e #t #f g h i)
       (== c h))))
 
-;Simulated nand, demonstrates classical logical completeness of {Friedkin gates with #t/#f constants}.
+;;  Simulated nand, demonstrates classical logical completeness of
+;;  {Friedkin gates with #t/#f constants}.
 (define nand-o
   (lambda (a b c)
     (fresh (d e f g h i)
@@ -50,7 +52,8 @@
       (3f-o e #t #f g h i)
       (== c h))))
 
-;Explosive not, partitions the expression domain into #f and {everything else}
+;;  Explosive not, partitions the expression domain into #f and
+;;  {everything else}
 (define enot-o
   (lambda (a b)
     (fresh (d e f g h i j k l)
@@ -58,22 +61,22 @@
       (3f-o g g h i #t j)
       (== b e))))
 
-;Explosive nand
+;;  Explosive nand
 (define enand-o
   (lambda (a b c)
     (fresh (d e f)
       (and-o a b d)
       (enot-o d c))))
 
-;Explosive nor
+;;  Explosive nor
 (define enor-o
   (lambda (a b c)
     (fresh (d e f)
       (or-o a b d)
       (enot-o d c))))
 
-;Extension to 4 field
-;(#f b c d) -> (#f d b c)
+;;  Extension to 4 field
+;;  (#f b c d) -> (#f d b c)
 
 #|
 (run 1 (q) (fresh (a b c d e f g h)
@@ -86,8 +89,8 @@
                  q)))
 |#
 
-;Extension to n-field
-;(#f a1 a2 ... an) -> (#f an a1 a2 ... an-1)
+;;  Extension to n-field
+;;  (#f a1 a2 ... an) -> (#f an a1 a2 ... an-1)
 
 (define nf-o
   (lambda (i o)
